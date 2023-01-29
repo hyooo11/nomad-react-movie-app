6f4c2fc069569c7a02b85745e4fa2fecb29313ff
hyooo11 <tlsgywls1107@naver.com>
2023-01-29 13:31:54 +0900

    to do list part one

--- src/App.js
+++ src/App.js
@@ -2,16 +2,20 @@ import { useState } from "react";
 
 function App() {
   const [toDo, setToDo] = useState("");
+  const [toDos, setToDos] = useState([]);
   const onChange = (event) => setToDo(event.target.value);
   const onSubmit = (event) => {
     event.preventDefault();
-    if(toDo===""){
+    if (toDo === "") {
       return;
     }
     setToDo("");
-  }
+    setToDos((currentArray) => [toDo, ...currentArray]);
+  };
+  console.log(toDos);
   return (
     <div>
+      <h1>My To Dos ({toDos.length})</h1>
       <form onSubmit={onSubmit}>
         <input
           onChange={onChange}
